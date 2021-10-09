1a858753208596e136f9dc00532c5b66ef62552d
lcp0578 <lcp0578@gmail.com>
2021-10-09 18:07:41 +0800

    fixed strtolower

--- Service/OpensslService.php
+++ Service/OpensslService.php
@@ -3,30 +3,30 @@ namespace Kit\CryptBundle\Service;
 
 class OpensslService
 {
-
+    
     private $clients;
-
+    
     /**
      *
-     * @param array $clients            
+     * @param array $clients
      */
     public function __construct($clients)
     {
         $this->clients = $clients;
     }
-
+    
     /**
      *
-     * @param string $string            
-     * @param string $iv            
+     * @param string $string
+     * @param string $iv
      * @return boolean|string
      */
     public function encrypt($string, $name = 'default', $iv = null)
     {
         // hash
-        $key = (strcasecmp('AES-256-CBC', $this->getMethod($name)) == 0) ? hash('sha256', $this->getSecretKey($name)) : $this->getSecretKey($name);
+        $key = ('aes-256-cbc' == $this->getMethod($name)) ? hash('sha256', $this->getSecretKey($name)) : $this->getSecretKey($name);
         $iv = ($iv === null) ? $this->getSecretIv($name) : $iv;
-        if (strcasecmp('AES-256-CBC', $this->getMethod($name)) == 0 && ! $this->checkIv($iv)) {
+        if ('aes-256-cbc' == $this->getMethod($name) && ! $this->checkIv($iv)) {
             return false;
         }
         if (! $this->checkMethod($name)) {
@@ -34,19 +34,19 @@ class OpensslService
         }
         return base64_encode(openssl_encrypt($string, $this->getMethod($name), $key, $this->getOption($name), $iv));
     }
-
+    
     /**
      *
-     * @param string $string            
-     * @param string $iv            
+     * @param string $string
+     * @param string $iv
      * @return boolean|string
      */
     public function decrypt($string, $name = 'default', $iv = null)
     {
         // hash
-        $key = (strcasecmp('AES-256-CBC', $this->getMethod($name)) == 0) ? hash('sha256', $this->getSecretKey($name)) : $this->getSecretKey($name);
+        $key = ('aes-256-cbc' == $this->getMethod($name)) ? hash('sha256', $this->getSecretKey($name)) : $this->getSecretKey($name);
         $iv = ($iv === null) ? $this->getSecretIv($name) : $iv;
-        if (strcasecmp('AES-256-CBC', $this->getMethod($name)) == 0 && ! $this->checkIv($iv)) {
+        if ('aes-256-cbc' == $this->getMethod($name) && ! $this->checkIv($iv)) {
             return false;
         }
         if (! $this->checkMethod($name)) {
@@ -54,19 +54,19 @@ class OpensslService
         }
         return openssl_decrypt(base64_decode($string), $this->getMethod($name), $key, $this->getOption($name), $iv);
     }
-
+    
     /**
      *
      * @return boolean
      */
     private function checkMethod($name = 'default')
     {
-        return in_array($this->getMethod($name), openssl_get_cipher_methods(true));
+        return in_array($this->getMethod($name), array_map('strtolower', openssl_get_cipher_methods(true)));
     }
-
+    
     /**
      *
-     * @param string $iv            
+     * @param string $iv
      * @return boolean
      */
     private function checkIv($iv)
@@ -74,62 +74,62 @@ class OpensslService
         // iv - encrypt method aes-256-cbc expects 16 bytes - else you will get a warning
         return strlen($iv) === 16;
     }
-
+    
     /**
      *
-     * @param string $name            
+     * @param string $name
      * @return string
      */
     private function getSecretKey($name)
     {
         return $this->getClient($name)['secret_key'];
     }
-
+    
     /**
      *
-     * @param string $name            
+     * @param string $name
      * @return string
      */
     private function getSecretIv($name)
     {
         return $this->getClient($name)['secret_iv'];
     }
-
+    
     /**
      *
-     * @param string $name            
+     * @param string $name
      * @return mixed
      */
     private function getMethod($name)
     {
-        return $this->getClient($name)['method'];
+        return strtolower($this->getClient($name)['method']);
     }
-
+    
     /**
      *
-     * @param string $name            
+     * @param string $name
      * @return mixed
      */
     private function getOption($name)
     {
         return $this->getClient($name)['option'];
     }
-
+    
     /**
      *
-     * @param string $name            
+     * @param string $name
      * @return array
      */
     private function getClient($name)
     {
         return isset($this->clients[$name]) ? $this->clients[$name] : $this->clients['default'];
     }
-
+    
     /**
      * Decrypt data from a CryptoJS json encoding string
      *
-     * @param mixed $passphrase            
-     * @param mixed $jsonString            
+     * @param mixed $passphrase
+     * @param mixed $jsonString
      * @return mixed
      */
     function cryptoJsAesDecrypt($passphrase, $ct, $iv, $s)
@@ -153,12 +153,12 @@ class OpensslService
         $data = openssl_decrypt($ct, 'aes-256-cbc', $key, true, $iv);
         return trim($data, '"');
     }
-
+    
     /**
      * Encrypt value to a cryptojs compatiable json encoding string
      *
-     * @param mixed $passphrase            
-     * @param mixed $value            
+     * @param mixed $passphrase
+     * @param mixed $value
      * @return string
      */
     function cryptoJsAesEncrypt($passphrase, $value)
